c54bca189b544611ed2322434ef79ca4bfba1c01
Borja Aday <borjagl@hotmail.com>
2023-11-05 23:01:24 +0100

    build TodoApp

--- src/main.jsx
+++ src/main.jsx
@@ -16,16 +16,17 @@ import {BrowserRouter} from 'react-router-dom'
 // import { CallBackHook } from './06-memos/CallBackHook'
 // import { Padre } from "./07-tarea-memo/Padre"
 // import './08-useReducer/intro-reducer'
-// import { TodoApp } from "./08-useReducer/TodoApp"
-import { MainApp } from './09-useContext/MainApp'
+// import { MainApp } from './09-useContext/MainApp'
+import { TodoApp } from "./08-useReducer/TodoApp"
+
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-        <MainApp />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>
+  // <BrowserRouter>
+    <React.StrictMode>
+        <TodoApp />
+    </React.StrictMode>
+  // </BrowserRouter>
   
 )
